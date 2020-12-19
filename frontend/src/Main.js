c7ecbdb5e4f57333528659b2b730f79c77dd32cc
arnav <arnav.jose@gmail.com>
2020-12-19 16:05:50 +0530

    next button

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,24 +1,30 @@
 import React from "react" 
 import Reservation from "./Form";
-import { Container } from "reactstrap";
+import { Container, NavbarText } from "reactstrap";
 
 class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          message:"",
+          page: 0,
           name: ""
         }
     }
     render(){
-        if(1){
+        if(this.state.page==0){
             var form = <Reservation />;
         }
+        else {
+            var form = <br />;
+        }
         return(
-            <Container className="mt-5 text-center">
+            <Container className="mt-5 text-justify bg-dark text-light">
                 {form}
+                <button onClick={()=>this.setState(state => ({page: state.page + 1}))}>
+                    Next {this.state.page}
+                </button>
             </Container>
-        )
+        );
     }
 }
 export default Main;
